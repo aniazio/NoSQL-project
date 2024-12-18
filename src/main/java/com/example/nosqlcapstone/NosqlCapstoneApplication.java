d22dfc21514fd5746ad21e77b62c069e2ef515ae
aziolkowska <aziolkowska@griddynamics.com>
2024-12-18 10:40:35 +0100

    add logging aspect

--- src/main/java/com/example/nosqlcapstone/NosqlCapstoneApplication.java
+++ src/main/java/com/example/nosqlcapstone/NosqlCapstoneApplication.java
@@ -2,8 +2,10 @@ package com.example.nosqlcapstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class NosqlCapstoneApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/nosqlcapstone/controller/TaskController.java
+++ src/main/java/com/example/nosqlcapstone/controller/TaskController.java
@@ -15,27 +15,23 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@Slf4j
 public class TaskController {
 
     private final TaskService taskService;
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
-        log.debug("Task controller; getAllUsers");
         return taskService.getAllUsers();
     }
 
     @GetMapping("/users/{userId}")
     public User getUser(@PathVariable String userId) {
-        log.debug(String.format("Task controller; getUser with id = %1s", userId));
         return taskService.getUser(userId);
     }
 
     @PostMapping("/users")
     @ResponseStatus(HttpStatus.CREATED)
     public void createUser(@RequestBody @Valid User user, HttpServletResponse response) {
-        log.debug(String.format("Task controller; createUser for user = %1s", user.toString()));
         User createdUser = taskService.createUser(user);
         response.addHeader("Location", String.format("/users/%1s", createdUser.getUserId()));
     }
@@ -43,32 +39,27 @@ public class TaskController {
     @PostMapping("/users/{userId}/tasks")
     @ResponseStatus(HttpStatus.CREATED)
     public void createTask(@PathVariable String userId, @RequestBody @Valid Task task, HttpServletResponse response) {
-        log.debug(String.format("Task controller; create task for userId = %1s and task = %2s", userId, task.toString()));
         Task created = taskService.createTask(userId, task);
         response.addHeader("Location", String.format("/users/%1s/tasks/%2s", userId, created.getTaskId()));
     }
 
     @GetMapping("/users/{userId}/tasks")
     public List<Task> getTasksForUser(@PathVariable String userId) {
-        log.debug(String.format("Task controller; getTasksForUser with userId = %1s", userId));
         return taskService.getTasksForUser(userId);
     }
 
     @GetMapping("/users/{userId}/tasks/{taskId}")
     public Task getTask(@PathVariable String userId, @PathVariable String taskId) {
-        log.debug(String.format("Task controller; getTask with userId = %1s and taskId = %2s", userId, taskId));
         return taskService.getTask(userId, taskId);
     }
 
     @PatchMapping("/users/{userId}/tasks/{taskId}")
     public Task updateStatus(@PathVariable String userId, @PathVariable String taskId, @RequestBody @Valid StatusDto newStatus) {
-        log.debug(String.format("Task controller; updateStatus with userId = %1s and taskId = %2s and new status = %3s", userId, taskId, newStatus.getStatus()));
         return taskService.updateStatus(userId, taskId, newStatus);
     }
 
     @DeleteMapping("/users/{userId}/tasks/{taskId}")
     public void deleteTask(@PathVariable String userId, @PathVariable String taskId) {
-        log.debug(String.format("Task controller; deleteTask with userId = %1s and taskId = %2s", userId, taskId));
         taskService.deleteTask(userId, taskId);
     }
 
